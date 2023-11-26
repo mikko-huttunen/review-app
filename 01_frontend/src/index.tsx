b32aa0100491fab6b4986eabcbcea92c751b5f7a
unknown <mikkohuttunen_92@proton.me>
2023-11-26 13:03:45 +0200

    Initial commit

---
+++ 01_frontend/src/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Keycloak from 'keycloak-js';
+
+let initOptions = {
+  url: 'http://localhost:8080/',
+  realm: 'review-app',
+  clientId: 'review-app-auth',
+  onLoad: 'check-sso', // check-sso | login-required
+  KeycloakResponseType: 'code',
+};
+
+let kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: 'login-required',
+  silentCheckSsoRedirectUri: window.location.origin + "/silent-check-sso.html", 
+  checkLoginIframe: false,
+  pkceMethod: 'S256'
+}).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    console.info("Authenticated");
+    console.log('auth', auth)
+    console.log('Keycloak', kc)
+    kc.onTokenExpired = () => {
+      console.log('token expired')
+    }
+  }
+}, () => {
+  console.error("Authenticated Failed");
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
